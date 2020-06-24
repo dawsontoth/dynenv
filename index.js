19597e5ecce20f4e8d7e33ae356c46e1de397401
Dawson Toth <dawsont@ifit.com>
2020-06-24 19:01:43 -0400

    fix: Properly resolve local requires

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const { spawn } = require('child_process');
+const { resolve } = require('path');
 
 const args = process.argv.slice(2);
 const barrier = args.indexOf('--');
@@ -9,7 +10,9 @@ const nextCommandArgs = args.slice(barrier + 2);
 const env = process.env;
 
 for (const fileToRequire of filesToRequire) {
-	const file = require(fileToRequire);
+	const file = require(fileToRequire.startsWith('./')
+		? resolve(fileToRequire)
+		: fileToRequire);
 	const newEnv = typeof file === 'function' ? file() : file;
 	Object.assign(env, newEnv);
 }
