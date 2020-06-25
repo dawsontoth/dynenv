9eca90d5b9284a4ee68e48b1ccaeb8e8602b9f35
Dawson Toth <dawsont@ifit.com>
2020-06-25 15:58:30 -0400

    feat: Support crawling node_modules for auto discovery

--- index.js
+++ index.js
@@ -1,23 +1,80 @@
 #!/usr/bin/env node
 const { spawn } = require('child_process');
-const { resolve } = require('path');
+const { readdir } = require('fs');
+const { join, resolve } = require('path');
 
-const args = process.argv.slice(2);
-const barrier = args.indexOf('--');
-const filesToRequire = args.slice(0, barrier);
-const nextCommand = args[barrier + 1];
-const nextCommandArgs = args.slice(barrier + 2);
-const env = process.env;
+parseArgs()
+	.then(state => requireFiles(state))
+	.then(state => spawnNextCommand(state))
+;
 
-for (const fileToRequire of filesToRequire) {
-	const file = require(fileToRequire.startsWith('./')
-		? resolve(fileToRequire)
-		: fileToRequire);
-	const newEnv = typeof file === 'function' ? file() : file;
-	Object.assign(env, newEnv);
+async function parseArgs() {
+	const args = process.argv.slice(2);
+	const barrier = args.indexOf('--');
+	if (-1 === barrier) {
+		const modules = await listDirectory('./node_modules/');
+		for (const parentDirectory of modules.filter(m => m.indexOf('/@') >= 0)) {
+			modules.push(...await listDirectory(parentDirectory));
+		}
+		const filesToRequire = [];
+		for (const module of modules) {
+			const packageJSON = safeRequire(resolve(module, 'package.json'));
+			if (packageJSON && packageJSON.dynenv) {
+				filesToRequire.push(resolve(module, packageJSON.dynenv));
+			}
+		}
+		return {
+			filesToRequire,
+			nextCommand: args[0],
+			nextCommandArgs: args.slice(1),
+		};
+	}
+	else {
+		return {
+			filesToRequire: args.slice(0, barrier),
+			nextCommand: args[barrier + 1],
+			nextCommandArgs: args.slice(barrier + 2),
+		};
+	}
 }
 
-spawn(nextCommand, nextCommandArgs, {
-	stdio: 'inherit',
-	env,
-});
+function listDirectory(ref) {
+	return new Promise((resolve, reject) => {
+		readdir(ref, (err, results) => err
+			? reject(err)
+			: resolve(results.map(result => join(ref, result))),
+		);
+	});
+}
+
+async function requireFiles(state) {
+	state.env = Object.assign({}, process.env);
+	for (const fileToRequire of state.filesToRequire) {
+		const exports = safeRequire(fileToRequire.startsWith('./')
+			? resolve(fileToRequire)
+			: fileToRequire);
+		const newEnv = typeof exports === 'function'
+			? await exports()
+			: exports;
+		if (newEnv) {
+			Object.assign(state.env, newEnv);
+		}
+	}
+	return state;
+}
+
+function safeRequire(ref) {
+	try {
+		return require(ref);
+	}
+	catch (err) {
+		return null;
+	}
+}
+
+function spawnNextCommand({ nextCommand, nextCommandArgs, env }) {
+	spawn(nextCommand, nextCommandArgs, {
+		stdio: 'inherit',
+		env,
+	});
+}
