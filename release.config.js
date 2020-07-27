477103b09645660cb95a57911008ea8990a17d07
Dawson Toth <dawsont@ifit.com>
2020-07-27 16:21:01 -0400

    fix: Commit package.json version

--- release.config.js
+++ release.config.js
@@ -2,6 +2,8 @@ module.exports = {
 	plugins: [
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
+		'@semantic-release/npm',
+		'@semantic-release/github',
 		[
 			'@semantic-release/git',
 			{
@@ -9,7 +11,5 @@ module.exports = {
 				'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
 			},
 		],
-		'@semantic-release/github',
-		'@semantic-release/npm',
 	],
 };
