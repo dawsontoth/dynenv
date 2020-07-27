907a0980430ccee0751f6c4f75bbb81567cebb2d
Dawson Toth <dawsont@ifit.com>
2020-07-27 16:18:37 -0400

    fix: Update release configuration

--- release.config.js
+++ release.config.js
@@ -1,9 +1,15 @@
 module.exports = {
 	plugins: [
 		'@semantic-release/commit-analyzer',
-		['@semantic-release/git', {
-			'assets': ['package.json'],
-			'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-		}],
+		'@semantic-release/release-notes-generator',
+		[
+			'@semantic-release/git',
+			{
+				'assets': ['package.json'],
+				'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+			},
+		],
+		'@semantic-release/github',
+		'@semantic-release/npm',
 	],
 };
