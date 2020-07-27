cbf2c245f18f87b3143fe6fe6ad82ccd54eb9ee5
Dawson Toth <dawsont@ifit.com>
2020-07-27 16:13:24 -0400

    fix: Expand semantic release plugins

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		['@semantic-release/git', {
+			'assets': ['package.json'],
+			'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+		}],
+	],
+};
